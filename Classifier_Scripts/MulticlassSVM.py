adac82e2b00158cb6b3f1996abb0636b9a181a4b
ishan <ipatwa@ufl.edu>
2015-02-10 15:58:54 -0500

    structred script

--- Classifier_Scripts/MulticlassSVM.py
+++ Classifier_Scripts/MulticlassSVM.py
@@ -11,7 +11,7 @@ from sklearn import svm
 #get the class labels from training set
 target =[];
 
-with open("testingData.txt","r+") as f:
+with open("trainingData.txt","r+") as f:
 		for line in f:
 			 target.append(int(line[0]))
 
@@ -22,7 +22,7 @@ with open("testingData.txt","r+") as f:
 counter =0
 samples =[ [] for i in range(len(target)) ]
 
-with open("testingData.txt","r+") as f:
+with open("trainingData.txt","r+") as f:
 	for lines in f:
 		line = lines.split()
 		for values in line[1:]:
@@ -30,7 +30,17 @@ with open("testingData.txt","r+") as f:
 
 		counter=counter+1
 			
+testing_features =[ [] for i in range(len(target)) ]
 
+counter = 0
+with open("testingData.txt","r+") as f:
+	for lines in f:
+		line = lines.split()
+		for values in line[1:]:
+				testing_features[counter].append(int(values))
+
+		counter=counter+1
+			
 print "samples length" 
 print len(samples)
 print "feature size" 
@@ -44,10 +54,14 @@ boston = datasets.load_boston()
 X, y = boston.data[:-1] , boston.target[:-1]
 
 clf = svm.SVC(probability=True)
-clf.fit(samples[:-1],target[:-1])
-probs =clf.predict_proba(samples[-1])
+clf.fit(samples,target)
+probs =clf.predict_proba(testing_features)
 print probs
-ans = clf.predict(samples[-1])
+ans = clf.predict(testing_features)
 print ans
 
+file = open("result.txt","r+")
+file.writelines(["%s\n" % item  for item in ans])
 
+file = open("result_probs.txt","r+")
+file.writelines(["%s\n" % item  for item in probs])
