02a28b1c8941cae501e52a450554b8b6cb494543
Ishan Patwa <Ishan@Ishans-MacBook-Pro.local>
2015-04-15 22:25:25 -0400

    parsed dr .kate dataset

--- Data Retrival/DataProcessing/retrieveAndLabelText.py
+++ Data Retrival/DataProcessing/retrieveAndLabelText.py
@@ -1,6 +1,7 @@
 
 import os
 import re
+from bs4 import BeautifulSoup
 
 
 TAG_RE = re.compile(r'<[^>]+>');
@@ -27,21 +28,101 @@ def remove_tags(text):
     return TAG_RE.sub('', text)
 
 def prepareTestData():
-	trainDir = '/Users/Ishan/Documents/WSD/Evaluation Datasets/Kate Saenko/bass';
+	#trainDir = '/Users/Ishan/Documents/WSD/Evaluation Datasets/Kate Saenko/bass';
+	trainDir = '/Users/Ishan/Documents/WSD/Evaluation Datasets/yahoo5_all/speaker/good';
+	finalPath = '/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/speaker/';
 	for dirPath,dirNames, files in os.walk(trainDir):
 	#format testing data i.e remove HTML tags and line breaks.
 			for file in files:
 					myFile  = os.path.join(dirPath,file);
-					fp = open(myFile,"r+");
-					f = open("test.txt","r+")
-					for i,line in enumerate(fp):
-								line = remove_tags(line);
-								line = line.strip('\t\r');
-								print line;
-								f.write(line);
-					fp.close();
-					f.close();
+					if ( myFile != "/Users/Ishan/Documents/WSD/Evaluation Datasets/yahoo5_all/speaker/good/.DS_Store"):
+										fp = open(myFile,"r+");
+										print myFile;
+										newfile = "text_" + file.replace('html','txt');
+										newfile = finalPath + newfile;
+										f = open(newfile,"a")
+										soup = BeautifulSoup(fp);
+										text = soup.get_text();
+										text = text + "\n";
+										f.write(text.encode('utf-8'));
 
 
-prepareTrainData();
-prepareTestData();
+
+#prepareTrainData();
+
+print "test data prepared..";
+def processTestData():
+	testDir = "/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/speaker";
+	finalPath = "/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/parsed speaker/"
+	for dirPath,dirNames, files in os.walk(testDir):
+			for file in files:
+					myFile = os.path.join(dirPath,file);
+					if ( myFile != "/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/speaker/.DS_Store"):
+								fp = open(myFile,"r");
+								newfile = "final_"+file.replace('html','txt');
+								newfile = finalPath + newfile;
+								for line in fp:
+											if not re.match(r'^\s*$', line):
+												line = re.sub(r"\s+", ' ', line);
+												f = open(newfile,"a");
+												f.write(line);
+								f.close();
+										  # Remove all non-word characters (everything except numbers and letters)
+			     					#line = re.sub(r"[^\w\s]", '', s);
+									      # Replace all runs of whitespace with a single dash
+									#line = re.sub(r"\s+", '-', s);
+
+
+def get_words_from_string(s):
+    return set(re.findall(re.compile('\w+'), s.lower()))
+
+
+def getwordsFromTestData():
+	testDir = "/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/parsed speaker";
+	finalPath ="/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/final_words_speaker/"
+	for dirPath,dirNames, files in os.walk(testDir):
+			for file in files:
+					myFile = os.path.join(dirPath,file);
+					print myFile;
+					if ( myFile != '/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/parsed speaker/.DS_Store'):
+								fp = open(myFile,"r");
+								newfile = "words_"+file.replace('html','txt');
+								newfile = finalPath + newfile;
+								for line in fp:
+										search_words = get_words_from_string(line);
+										newline  = ' '.join(search_words);
+										f = open(newfile,"a");
+										f.write(newline);
+								f.close();
+
+
+#prepareTestData();
+#processTestData();
+#print "test data processed fetching words!"
+#getwordsFromTestData();
+
+
+def mergeFInalWords():
+	srcDir = "/Users/Ishan/Documents/WSD/Evaluation Datasets/Dr. Saneko Parsed Dataset/final_words_bass/";
+	f = open("test.txt","a");
+	for dirPath, dirNames, files in os.walk(srcDir):
+		for file in files:
+				myFile = os.path.join(dirPath,file);
+				fp = open(myFile,"r");
+				for line in fp:
+						line = file + " " + line ;
+						line = line + "\n";
+						f.write(line);
+
+
+mergeFInalWords();
+
+
+
+
+
+
+
+
+
+
--- Data Retrival/HTML_parsers/HTMLparser.php
+++ Data Retrival/HTML_parsers/HTMLparser.php
@@ -22,25 +22,50 @@ foreach ( $dirIterator as $file){
 
 						foreach( $body as $tag){
 									$children_of_tag = $tag->children();
+									var_dump($tag->plaintext);
 									//cvar_dump($children_of_tag);
 									foreach( $children_of_tag as $last_line){
 												if ( $last_line->tag == 'p'){
-														$mtString = $mtString . $last_line->plaintext;
+														$mtString = $mtString . $last_line->outertext;
 														$pattern = '/&nbsp;/';
 														$pattern2 = '/&amp;/';
 														$pattern3 = '/&quot;/';
 														$mtString = preg_replace($pattern,'', $mtString);
 														$mtString = preg_replace($pattern2,'', $mtString);
 														$mtString = preg_replace($pattern3,'', $mtString);
-														print $mtString;
+														$mtString = strip_tags($mtString);
+													//	print $mtString;
 												}
 
 									}
 						}
-						$pattern = '/\s/';
-						$mtString = preg_replace($pattern, " ", $mtString);
+
+						$tbody = $html->find('tbody');
+						foreach( $tbody as $tag){
+									$children_of_tag = $tag->children();
+									var_dump($tag->plaintext);
+									//cvar_dump($children_of_tag);
+									foreach( $children_of_tag as $last_line){
+												if ( $last_line->tag == 'p'){
+														$mtString = $mtString . $last_line->outertext;
+														$pattern = '/&nbsp;/';
+														$pattern2 = '/&amp;/';
+														$pattern3 = '/&quot;/';
+														$mtString = preg_replace($pattern,'', $mtString);
+														$mtString = preg_replace($pattern2,'', $mtString);
+														$mtString = preg_replace($pattern3,'', $mtString);
+														$mtString = strip_tags($mtString);
+													//	print $mtString;
+												}
+
+									}
+						}
+						//$pattern = '/\s/';
+						//$mtString = preg_replace($pattern, " ", $mtString);
 						$newFilename = $file->getFilename();
+						$newFilename = str_replace('html', 'txt', $newFilename);
 						$newFilename = "NEW" .$newFilename;
+						//echo $newFilename."\n";
 						file_put_contents($newFilename, $mtString);
 			}
 		}
