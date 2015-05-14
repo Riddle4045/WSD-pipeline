a99fb37464c18626e3981a31d809044f5b52354f
SunPHM <pengyang823@gmail.com>
2015-05-14 00:21:55 -0400

    minor fix

--- mwsd/text_process.py
+++ mwsd/text_process.py
@@ -1,10 +1,9 @@
 import os
-folder = "/home/yp/projects/data/uiuc/bass"
+folder = "/home/yp/projects/data/uiuc/squash"
 dirPath = folder + "/img/test";
 f = open(folder + "/txt/test.txt","w");
 for dirName, subdirList, fileList in os.walk(dirPath):
-			for file in fileList:
-							file = file.replace("_"," ");
-							f.write(file[2:-4] + "\n");
-
+	for p in fileList:
+		p = p.replace("_"," ");
+		f.write(p[2:-4] + "\n");
 f.close()
