36fbf2e590cd0f47f655fd8d5427eab2c8f9899c
SunPHM <pengyang823@gmail.com>
2015-05-18 21:17:11 -0400

    fix on sense_dist to discard distribution

--- mwsd/txt/sense_dist.py
+++ mwsd/txt/sense_dist.py
@@ -9,79 +9,42 @@ from decimal import Decimal
 from numpy import zeros
 import time as dt
 
-def main(word):
+def main(argv):
+	word = argv[1]
+	seed = argv[2]
+	train = argv[3]
+	test = argv[4]
+	folder = argv[5]
+	classifier = folder + "/" + word + ".classifier"
+	results = folder + "/" + word + ".disambiguated"
 	#get definition of word from WordNet.
 	print 'Word to be disambiguated: %s. The senses in the WordNet are:' % word
-	if os.path.isfile(word) == False:
-		print 'Error: the input file "'+word+'" does not exist.'
+	if os.path.isfile(seed) == False or os.path.isfile(train) == False or os.path.isfile(test) == False:
+		print 'Error: the input file(s) does not exist.'
 		sys.exit(2)
-	ss = wn.synsets(word,pos="n")
-	for i in range (0,len(ss)):
-		print str(i+1) + ': ' + ss[i].definition()
-	print '-----------------------------'
 	#Train classifier
 	n1=dt.time()
-	sys.stdout.write('Generating seeds ... ')
-	fd = open(word+'.seed', 'w');
-	fd.truncate()
-	for i in range (0,len(ss)):
-		fd.write(ss[i].definition()+'\n')
-	fd.close()
-	n2=dt.time()
-	print 'done. Elapsed time is %.4f seconds.' % ((n2-n1))
-	n1=dt.time()
 	sys.stdout.write('Training classifier ... ')
-	cmd = './uwsd -train '+word+' '+word+'.seed'+' '+word+'.classifier'+' ' + word
+	cmd = './uwsd -train '+ train + ' '+ seed +' '+ classifier+' ' + word
 	os.system(cmd)
 	n2=dt.time()
 	print 'done. Elapsed time is %.4f seconds.' % ((n2-n1))
 	n1=dt.time()
 	#Classify the text
 	sys.stdout.write('Classifying text ... ')
-	cmd = './uwsd -test '+word+'.classifier'+' '+word+' ' + word
+	cmd = './uwsd -test '+ classifier +' '+ test +' ' + word
 	out = subprocess.check_output(cmd, shell=True)
-	fd = open(word+'.disambiguated', 'w');
+	fd = open(results, 'w');
 	fd.truncate()
 	fd.write(out)
 	fd.close()
 	n2=dt.time()
 	print 'done. Elapsed time is %.4f seconds.' % ((n2-n1))
-	#Print sense distribution.
-	fd_in_text = open(word, 'r');
-	in_text = fd_in_text.read().split('\n');
-	in_text_label = zeros(len(in_text));
-	out = out.split('\n');
-	print '-----------------------------'
-	hist = zeros(len(ss)+1)
-	for i in range(0,len(out)):
-		if len(out[i])>1:
-			t = Decimal(out[i].split(' ')[0])
-			if t==-1:
-				hist[0] += 1
-				in_text_label[i] = 0;
-			else:
-				hist[t] += 1
-				in_text_label[i] = t;
-	print 'Total number of %d lines were processed. The sense distribution is:' % (sum(hist))
-	hist = hist/sum(hist);
-	for i in range(0,len(hist)):
-		if i==0:
-			print 'N/A:      %2d%%' % (100*hist[0])
-		else:
-			print'Sense %2d: %2d%%' % (i,100*hist[i])
-		if hist[i]<0:
-			cnt = 1
-			for k in range(0,len(in_text)):
-				if len(in_text[k])>1 and in_text_label[k]==i:
-					print in_text[k]
-					cnt += 1
-				if cnt >3:
-					break;
 
 				
 				
 if __name__ == "__main__":
-	if len(sys.argv)  != 2:
-		print 'Need to specify the word to be disambiguated.';
+	if len(sys.argv)  != 6:
+		print 'Not enough arguments.';
 		sys.exit(2);
-	main(sys.argv[1])
+	main(sys.argv)
